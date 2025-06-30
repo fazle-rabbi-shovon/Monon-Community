839df4b1cf544e36ff2091d331a0d51440083f21
shovon <shovon@inovacetech.com>
2025-06-30 09:43:11 +0600

    meditation video added

--- lib/ui/video/video_player.dart
+++ lib/ui/video/video_player.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 import '../../route/navigation_service.dart';
@@ -7,14 +9,12 @@ class VideoPlayerScreen extends StatefulWidget {
   final String title;
   final String url;
   final String imagePath;
-  final String duration;
 
   const VideoPlayerScreen({
     Key? key,
     required this.title,
     required this.url,
     required this.imagePath,
-    required this.duration,
   }) : super(key: key);
 
   @override
@@ -24,19 +24,58 @@ class VideoPlayerScreen extends StatefulWidget {
 class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
   late VideoPlayerController _controller;
   bool _isControlsVisible = true;
-  bool _wasPlayingBeforeHide = false;
+  bool _isInitialized = false;
+  Duration watchedDuration = Duration.zero;
+  bool hasSavedToFirebase = false;
 
   @override
   void initState() {
     super.initState();
     _controller = VideoPlayerController.network(widget.url)
       ..initialize().then((_) {
-        setState(() {});
+        setState(() {
+          _isInitialized = true;
+        });
       });
     _controller.addListener(() {
       setState(() {});
     });
+
+    _controller.addListener(() {
+      if (_controller.value.isPlaying && _isInitialized) {
+        watchedDuration = _controller.value.position;
+
+        if (!hasSavedToFirebase && watchedDuration >= const Duration(seconds: 2)) {
+          hasSavedToFirebase = true;
+          // _saveVideoWatchedToFirebase();
+        }
+      }
+
+      setState(() {});
+    });
+
   }
+
+  Future<void> _saveVideoWatchedToFirebase() async {
+    final uid = FirebaseAuth.instance.currentUser?.uid;
+    if (uid == null) return;
+
+    final now = DateTime.now();
+    final formattedDate =
+        "${now.year}-${now.month.toString().padLeft(2, '0')}-${now.day.toString().padLeft(2, '0')}";
+
+    final docRef = FirebaseFirestore.instance
+        .collection('kisu_kotha')
+        .doc(uid)
+        .collection(formattedDate)
+        .doc(widget.title); // Using video title as unique doc
+
+    await docRef.set({
+      "দেখা হয়েছে": "হ্যাঁ",
+      "watchedAt": FieldValue.serverTimestamp(),
+    });
+  }
+
 
   @override
   void dispose() {
@@ -65,11 +104,11 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
         child: Column(
           children: [
             const SizedBox(height: 24),
-            _controller.value.isInitialized
-                ? GestureDetector(
-              onTap: _toggleControls,
-              child: AspectRatio(
-                aspectRatio: _controller.value.aspectRatio,
+            AspectRatio(
+              aspectRatio: _controller.value.aspectRatio,
+              child: _isInitialized
+                  ? GestureDetector(
+                onTap: _toggleControls,
                 child: Stack(
                   alignment: Alignment.center,
                   children: [
@@ -78,21 +117,18 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
-                          if (!_controller.value.isPlaying)
-                            IconButton(
-                              icon: const Icon(Icons.fast_rewind, size: 36, color: ColorUtil.mainColor),
-                              onPressed: () {
-                                _controller.seekTo(
-                                  _controller.value.position - const Duration(seconds: 10),
-                                );
-                              },
-                            ),
+                          IconButton(
+                            icon: const Icon(Icons.fast_rewind, size: 36, color: ColorUtil.mainColor),
+                            onPressed: () {
+                              _controller.seekTo(_controller.value.position - const Duration(seconds: 10));
+                            },
+                          ),
                           IconButton(
                             iconSize: 48,
                             icon: Icon(
                               _controller.value.isPlaying
                                   ? Icons.pause_circle_filled
-                                  : Icons.play_circle,
+                                  : Icons.play_circle_filled,
                               color: ColorUtil.mainColor,
                             ),
                             onPressed: () {
@@ -103,49 +139,54 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
                               });
                             },
                           ),
-                          if (!_controller.value.isPlaying)
-                            IconButton(
-                              icon: const Icon(Icons.fast_forward, size: 36, color: ColorUtil.mainColor),
-                              onPressed: () {
-                                _controller.seekTo(
-                                  _controller.value.position + const Duration(seconds: 10),
-                                );
-                              },
-                            ),
+                          IconButton(
+                            icon: const Icon(Icons.fast_forward, size: 36, color: ColorUtil.mainColor),
+                            onPressed: () {
+                              _controller.seekTo(_controller.value.position + const Duration(seconds: 10));
+                            },
+                          ),
                         ],
                       ),
-                    Positioned(
-                      bottom: 8,
-                      left: 12,
-                      child: Text(
-                        _formatTime(_controller.value.position),
-                        style: const TextStyle(color: ColorUtil.mainColor, fontSize: 12),
+                    if (_isControlsVisible) ...[
+                      Positioned(
+                        bottom: 8,
+                        left: 12,
+                        child: Text(
+                          _formatTime(_controller.value.position),
+                          style: const TextStyle(color: ColorUtil.mainColor, fontSize: 12),
+                        ),
                       ),
-                    ),
-                    Positioned(
-                      bottom: 8,
-                      right: 12,
-                      child: Text(
-                        _formatTime(_controller.value.duration),
-                        style: const TextStyle(color: ColorUtil.mainColor, fontSize: 12),
+                      Positioned(
+                        bottom: 8,
+                        right: 12,
+                        child: Text(
+                          _formatTime(_controller.value.duration),
+                          style: const TextStyle(color: ColorUtil.mainColor, fontSize: 12),
+                        ),
                       ),
-                    ),
+                    ],
                   ],
                 ),
-              ),
-            )
-                : const CircularProgressIndicator(),
+              ) :
+              const Center(child: CircularProgressIndicator()),
+            ),
+            const SizedBox(height: 10),
             Slider(
-              value: _controller.value.position.inSeconds.toDouble(),
+              value: _isInitialized
+                  ? _controller.value.position.inSeconds.toDouble()
+                  : 0,
               min: 0,
-              max: _controller.value.duration.inSeconds.toDouble(),
-              onChanged: (value) {
+              max: _isInitialized
+                  ? _controller.value.duration.inSeconds.toDouble()
+                  : 1,
+              onChanged: _isInitialized
+                  ? (value) {
                 _controller.seekTo(Duration(seconds: value.toInt()));
-              },
+              }
+                  : null,
               activeColor: ColorUtil.mainColor,
               inactiveColor: Colors.grey,
             ),
-            const SizedBox(height: 10),
           ],
         ),
       ),
@@ -158,11 +199,19 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
       centerTitle: true,
       backgroundColor: ColorUtil.primary,
       leading: IconButton(
-        icon: const Icon(Icons.close, color: Colors.white),
+        icon: const Icon(Icons.arrow_back, color: Colors.white),
         onPressed: () {
           NavigationService.getCurrentState()?.pop();
         },
       ),
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.close, color: Colors.white),
+          onPressed: () {
+            NavigationService.getCurrentState()?.pop();
+          },
+        ),
+      ],
     );
   }
 }
